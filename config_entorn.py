9d9d585b891f7430d8875dc3e215dff918850ec6
ferdeveloper05 <fer.developer05@gmail.com>
2026-09-15 22:33:38 -0400

    refact: funcion validate_path_dirs, agregando el retorno de archivos

--- config_entorn.py
+++ config_entorn.py
@@ -24,19 +24,21 @@ def get_paramm_input() -> str:
     
     return paramm
 
-def validate_path_dirs(path_dest: str, path_origin: str): 
+def validate_path_dirs(path_dest: str, path_origin: str) -> list:
+    """ Valida la ruta de origen y devuelve una lista con los archivos de esta """
+     
     dir_dest = Path.home() / path_dest
     dir_origin = Path.home() / path_origin
     
-    print(f"Ruta Destino: {dir_dest}")
-    print(f"Ruta Origen: {dir_origin}")
+    print(f"\nRuta Destino: {dir_dest}")
+    print(f"Ruta Origen: {dir_origin}\n")
     
     if dir_origin.exists() and dir_origin.is_dir(): 
         dir_dest.mkdir(parents=True, exist_ok=True)
         
-        for file in dir_origin.rglob('*'): 
-            print(file)
+        files = [file for file in dir_origin.rglob("*")]
     
+    return files, dir_origin
 
 if __name__ == "__main__": 
 
