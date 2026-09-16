ec8ce76e62807f1a07b030144f339797fd566a70
ferdeveloper05 <fer.developer05@gmail.com>
2026-09-15 21:46:43 -0400

    Feat: Conexion con base de datos

---
+++ file_databases.py
@@ -0,0 +1,22 @@
+import sqlite3
+from pathlib import Path
+
+
+def create_file_databases(name_db: str, ext: str = '.db'): 
+    concat_name = name_db + ext
+    name_database = Path.home() / concat_name
+    name_database.touch(exist_ok=True)
+    
+    return name_database
+
+def execute_database(name_database): 
+    conn = sqlite3.connect(name_database)
+
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE archivos (id INTEGER PRIMARY KEY AUTOINCREMENT, nombre TEXT, hash_sha256 TEXT UNIQUE, ruta_origen TEXT, fecha_procesado TIMESTAMP DEFAULT CURRENT_TIMESTAMP)')
+    conn.commit()
+    
+    conn.close()
+
+if __name__ == "__main__":
+    database = create_file_databases("Code/register_files")
